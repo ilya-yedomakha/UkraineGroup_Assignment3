037d067507dd24668dfa76dbf6dfd2450a7b56dc
Ilya Yedomakha <yedomakhailia@gmail.com>
2024-12-10 21:17:28 +0200

    salePerformanceRecord receiving from CRX before current year only

--- backend/src/apis/sale_performance_records_controller.js
+++ backend/src/apis/sale_performance_records_controller.js
@@ -1,13 +1,15 @@
+// noinspection LanguageDetectionInspection
+
 const salesmanModel = require("../models/SalesMan")
 const socialPerformanceRecordModel = require("../models/SocialPerformanceRecord")
 const salePerformanceRecordModel = require("../models/SalePerformanceRecord")
+
 const salesmanService = require("../services/salesman-service")
 
 const axios = require('axios');
 const qs = require('qs');
 
-//TODO зроби так, щоб по масиву salesOrderPositions проходилось форічем і робило запити в кожному рядку і вписувались дані про продукт, продавця і
-// клієнта і зберегти це все в таблицю salePerformance-ів І додати поточний рік. Ще подумайте над тим, як працювати з полем SaleOrder: Active/Valid from:
+//TODO Ще подумайте над тим, як працювати з полем SaleOrder: Active/Valid from:
 // 2/12/2018, 12:57:44 PM. Оскільки бонуси розраховуються для поточного року.
 
 class sale_performance_record_controller {
@@ -28,11 +30,10 @@ class sale_performance_record_controller {
 
             const salesOrders = response.data.objects;
 
-            const salesOrderPositions = [];
+            const salesPerformanceRecords = [];
 
             for (const salesOrder of salesOrders) {
                 const href = salesOrder["@href"];
-
 
                 const positionResponse = await axios.get(
                     `${href}/position`,
@@ -47,20 +48,57 @@ class sale_performance_record_controller {
                     }
                 );
 
-
-
                 const positions = positionResponse.data.objects || [];
-                positions.forEach((position) => {
-                    salesOrderPositions.push({
+                for (const position of positions) {
+                    const { salesRep: salesmanHref, customer: clientHref } = salesOrder;
+                    const { product: productHref } = position;
+
+                    const salesmanResponse = await axios.get(salesmanHref["@href"], {
+                        auth: {
+                            username: 'guest',
+                            password: 'guest',
+                        },
+                        headers: {
+                            'Accept': 'application/json',
+                        },
+                    });
+                    const salesman = salesmanResponse.data;
+
+                    const clientResponse = await axios.get(clientHref["@href"], {
+                        auth: {
+                            username: 'guest',
+                            password: 'guest',
+                        },
+                        headers: {
+                            'Accept': 'application/json',
+                        },
+                    });
+                    const client = clientResponse.data;
+
+                    const productResponse = await axios.get(productHref["@href"], {
+                        auth: {
+                            username: 'guest',
+                            password: 'guest',
+                        },
+                        headers: {
+                            'Accept': 'application/json',
+                        },
+                    });
+                    const product = productResponse.data;
+
+                    salesPerformanceRecords.push({
                         "salePerformanceHref": salesOrder["@href"],
                         "salesOrderName": salesOrder.name,
                         "priority": salesOrder.priority,
-                        "salesmanHref": salesOrder.salesRep,
-                        "clientHref": salesOrder.customer,
+                        "salesmanHref": salesman["@href"],
+                        "salesmanGovId": salesman.governmentId,
+                        "clientHref": client["@href"],
+                        "clientRating": client.accountRating,
+                        "clientFullName": client.fullName,
                         "positionHref": position["@href"],
-                        "positionNumber": position.positionNumber,
                         "positionLineItemNumber": position.lineItemNumber,
                         "positionName": position.name,
+                        "positionNumber": position.positionNumber,
                         "positionPricePerUnit": position.pricePerUnit,
                         "positionQuantity": position.quantity,
                         "positionPricingStatus": position.pricingState,
@@ -68,20 +106,20 @@ class sale_performance_record_controller {
                         "positionDiscountAmount": position.discountAmount,
                         "positionTaxAmount": position.taxAmount,
                         "positionTotalAmountInclTax": position.amount,
-                        "productHref": position.product
-
-
+                        "productHref": product["@href"],
+                        "productNumber": product.productNumber,
+                        "productName": product.name,
                     });
-                });
+                }
             }
 
-            res.status(200).send({ salesOrderPositions });
+            await salePerformanceRecordModel.insertMany(salesPerformanceRecords);
+
+            res.status(200).send({ salesPerformanceRecords });
         } catch (e) {
             res.status(500).send({ message: e.message, data: e });
         }
     };
-
-
 
 
 }
